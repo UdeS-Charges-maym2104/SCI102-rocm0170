b53c10509cad5956c7dc7cc1ed63cd1b7dd6d3fc
rocm0170 <rocm0170@usherbrooke.ca>
2025-09-16 14:44:39 -0400

    Correction de l'erreur de concaténation dans la fonction age

--- labo2.py
+++ labo2.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def age(_annee_naissance_):
-    return "Votre âge est " + datetime.now().year - _annee_naissance_ + "ans ."
+    return "Votre âge est " + str(datetime.now().year - _annee_naissance_) + "ans ."
 
 def salutations(_nom_):
     return "Bonjour " + _nom_ + "."
